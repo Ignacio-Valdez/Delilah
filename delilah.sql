042ac37072ea92a3254226a2ef4f4924cf145c58
Ignacio-Valdez <60761792+Ignacio-Valdez@users.noreply.github.com>
2020-07-23 23:07:15 -0300

    Agregando 3er proy

---
+++ delilah.sql
@@ -0,0 +1,88 @@
+CREATE TABLE usuarios (
+    id_usuario INT PRIMARY KEY AUTO_INCREMENT,
+    nombre_usuario VARCHAR(60) NOT NULL,
+    nombre_apellido VARCHAR (100) NOT NULL,
+    email VARCHAR (60) NOT NULL,
+    telefono INT NOT NULL,
+    direccion VARCHAR(200) NOT NULL,
+    contrasena VARCHAR(100) NOT NULL,
+    es_admin INT NOT NULL
+    );
+CREATE TABLE productos (
+    id_producto INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(60) NOT NULL,
+    imagen VARCHAR (500) NOT NULL,
+    precio FLOAT NOT NULL
+    );
+CREATE TABLE pedidos (
+    id_pedido INT PRIMARY KEY AUTO_INCREMENT,
+    hora DATETIME NOT NULL,
+    estado VARCHAR (60) NOT NULL,
+    total INT NOT NULL,
+    metodo_pago VARCHAR (60) NOT NULL,
+    id_usuario INT NOT NULL
+    );
+CREATE TABLE productos_x_pedidos (
+    id_pro_pe INT PRIMARY KEY AUTO_INCREMENT,
+    id_producto INT NOT NULL,
+    id_pedido INT NOT NULL,
+    cantidad INT NOT NULL
+    );
+
+INSERT INTO usuarios VALUES (NULL, 'queen_freddie', 'Freddie Mercury', 'freddiemercury@gmail.com', 123456789, '1 Logan PIKensington', 'Freddie123',0);
+INSERT INTO usuarios VALUES (NULL, 'indie_john', 'John Smith', 'johnsmith@gmail.com', 234567891, 'Tardis ST, space 453, Gallifrey', 'John123',0);
+INSERT INTO usuarios VALUES (NULL, 'queen_brian', 'Brian May', 'brianmay@gmail.com', 345678912, '8236 Bohemian Stret, Paddington, London B.', 'Briam123',0);
+INSERT INTO usuarios VALUES (NULL, 'queen_malin', 'Malin Quist', 'malinquist@mail.com', 456789123, '246 Jefrey Gateway', 'Malin123',0);
+INSERT INTO usuarios VALUES (NULL, 'jordanakit', 'Jordana Kitchener', 'jordanakitchener@gmail.com', 456715223, '5157 Grady Ridge', 'Jordana123',0);
+INSERT INTO usuarios VALUES (NULL, 'ivansan', 'Ivan Sanders', 'ivansanders@gmail.com', 567891234, '81 Conrad Plain', 'Ivan123',0);
+INSERT INTO usuarios VALUES (NULL, 'mribero', 'Mónica Ribeiro', 'monicaribeiro@gmail.com', 678912345, '98 Brendon Street Suite 314', 'Monica123',0);
+INSERT INTO usuarios VALUES (NULL, 'Klove', 'Katie Love', 'katielove@gmail.com', 789123456, '245 Turner Spring Apt.010', 'Katie123',0);
+INSERT INTO usuarios VALUES (NULL, 'IVasin', 'Ilya Vasin', 'iliavasin@gmail.com', 891234567, '3955 Dejon Green Apt. 553', 'Ilia123',0);
+INSERT INTO usuarios VALUES (NULL, 'AnnaF', 'Anna Fali', 'annafali@gmail.com', 912345678, '593 Dessie Manors', 'Anna123',0);
+INSERT INTO usuarios VALUES (NULL, 'HBriscam', 'Hashim Briscam', 'hashimbriscam@gmail.com', 012345678, 'Davies Green', 'Hashim123',0);
+INSERT INTO usuarios VALUES (NULL, 'AntonB', 'Anton Brownstein', 'antonbrownstein@gmailcom', 123654987, '6254 McDemott Trafficway', 'Anton123',0);
+INSERT INTO usuarios VALUES (NULL, 'DayaC', 'Daya Chitanis', 'danachitanis@gmail.com', 123456987, '9178 Reinhold Island', 'Daya123',0);
+INSERT INTO usuarios VALUES (NULL, 'ConstanzaM', 'Constanza Mariano', 'constanzamariano@gmail.com', 456321852, '026 Harber Port', 'Constanza123',0);
+INSERT INTO usuarios VALUES (NULL, 'DeboraAll', 'Débora Barbosa', 'deborabarbosa@gmail.com', 456963159, '713 Howe Crossing', 'Debora123',0);;
+INSERT INTO usuarios VALUES (NULL, 'SafiyulinB', 'Benedikt Safiyulin', 'benediktsafiyulin@gmail.com', 654357985, '821 Hansen Landing Suite 633', 'Safiyulin123',0);
+INSERT INTO usuarios VALUES (NULL, 'Delilah', 'Delilah Resto', 'delilahresto@gmail.com', 457327696, '351 Muller ST', 'Delilah123',1);
+
+INSERT INTO productos VALUES (NULL, 'Bagel de salmon', 'http://DR/images/salmon',425);
+INSERT INTO productos VALUES (NULL, 'Hamburguesa clásica', 'http://DR/images/hamb_clas',350);
+INSERT INTO productos VALUES (NULL, 'Hamburguesa Especial', 'http://DR/images/hamb_clas_esp',380);
+INSERT INTO productos VALUES (NULL, 'Sandwich veggie', 'http://DR/images/sand_ve', 310);
+INSERT INTO productos VALUES (NULL, 'Ensalada veggie', 'http://DR/images/ens_ve', 340);
+INSERT INTO productos VALUES (NULL, 'Focaccia', 'http://DR/images/focca', 300);
+INSERT INTO productos VALUES (NULL, 'Sandwich Focaccia', 'http://DR/images/sand-focca', 440);
+INSERT INTO productos VALUES (NULL, 'Sandwich Queso', 'http://DR/images/sand-queso', 268);
+INSERT INTO productos VALUES (NULL, 'Cerveza Lata', 'http://DR/images/cerv-lata', 110);
+INSERT INTO productos VALUES (NULL, 'Gaseosa Lata ', 'http://DR/images/gaseo-lata', 100);
+INSERT INTO productos VALUES (NULL, 'Agua', 'http://DR/images/agua', 90);
+INSERT INTO productos VALUES (NULL, 'Pizza Especial', 'http://DR/images/pizza-esp', 450);
+INSERT INTO productos VALUES (NULL, 'Pizza Muzzarella', 'http://DR/images/pizza-c', 420);
+
+INSERT INTO pedidos VALUES (NULL, '2020-06-25 10:46:36', 'Preparando', 670, 'Efectivo', 1);
+INSERT INTO pedidos VALUES (NULL, '2020-06-25 10:58:36', 'Confirmado', 525, 'Tarjeta', 5);
+INSERT INTO pedidos VALUES (NULL, '2020-06-25 11:06:36', 'Preparando', 1860, 'Tarjeta', 10);
+INSERT INTO pedidos VALUES (NULL, '2020-06-25 11:08:36', 'Enviando', 900, 'Efectivo', 8);
+INSERT INTO pedidos VALUES (NULL, '2020-06-25 11:58:36', 'Cancelado', 90, 'Efectivo', 13);
+INSERT INTO pedidos VALUES (NULL, '2020-06-25 12:10:36', 'Entregado', 660, 'Efectivo', 5);
+INSERT INTO pedidos VALUES (NULL, '2020-06-25 21:58:36', 'Preparado', 368, 'Efectivo', 6);
+
+INSERT INTO productos_x_pedidos VALUES (NULL, 12, 1, 1);
+INSERT INTO productos_x_pedidos VALUES (NULL, 9, 1, 2);
+INSERT INTO productos_x_pedidos VALUES (NULL, 1, 2, 1);
+INSERT INTO productos_x_pedidos VALUES (NULL, 10, 2, 1);
+INSERT INTO productos_x_pedidos VALUES (NULL, 4, 3, 6);
+INSERT INTO productos_x_pedidos VALUES (NULL, 2, 4, 2);
+INSERT INTO productos_x_pedidos VALUES (NULL, 10, 4, 2);
+INSERT INTO productos_x_pedidos VALUES (NULL, 11, 5, 1);
+INSERT INTO productos_x_pedidos VALUES (NULL, 5, 6, 2);
+INSERT INTO productos_x_pedidos VALUES (NULL, 9, 6, 2);
+INSERT INTO productos_x_pedidos VALUES (NULL, 10, 6, 1);
+INSERT INTO productos_x_pedidos VALUES (NULL, 8, 7, 1);
+INSERT INTO productos_x_pedidos VALUES (NULL, 10, 7, 1);
+
+ALTER TABLE pedidos ADD FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario);
+ALTER TABLE productos_x_pedidos ADD FOREIGN KEY (id_producto) REFERENCES productos(id_producto);
+ALTER TABLE productos_x_pedidos ADD FOREIGN KEY (id_pedido) REFERENCES pedidos(id_pedido);
---
+++ index.js
@@ -0,0 +1,356 @@
+const jwt = require ('jsonwebtoken');
+const express = require ('express');
+const bodyParser = require ('body-parser');
+const Sequelize = require ('sequelize');
+const { response } = require('express');
+const sequelize = new Sequelize ('mysql://root:@localhost:3306/delilah_resto');
+const app = express();
+
+app.use(bodyParser.json());
+const passwordJwt= 'DeliRes351';
+
+//PRODUCTOS
+
+//Mostrar todos los productos
+
+app.get('/api/productos', ensureToken, (req, res)=> {
+    const query = 'SELECT * FROM productos';
+    jwt.verify(req.token, passwordJwt, (error, data) => {
+        if (error) {
+            res.sendStatus(403).json('Debe iniciar sesión');
+        } else {
+            sequelize.query(query, {type: sequelize.QueryTypes.SELECT})
+                .then((response)=>{
+                    res.status(200).json(response);
+                }).catch((e) => res.status(400).json({error: 'No se pueden visualizar productos'}));
+            }
+        })
+    });
+
+//Mostrar producto por id
+
+app.get('/api/productos/:id', ensureToken,(req, res)=> {
+    const id = req.params.id;
+    const query = 'SELECT * FROM productos WHERE id_producto = ?';
+    jwt.verify(req.token, passwordJwt, (error, data) => {
+        if (error) {
+            res.sendStatus(403).json('Debes iniciar sesión');
+        } else {
+           sequelize.query(query, {replacements: [id], type: sequelize.QueryTypes.SELECT})
+            .then((response)=>{
+                if(response.length){
+                    res.json(response);
+                    console.log(response);
+                }else{
+                    res.status(404).send('No existe el producto')
+                }
+            }).catch((e) => res.status(400).json({error: 'No se puede mostrar el producto'}));
+        }
+    })    
+});
+
+//Agregar productos
+
+app.post('/api/productos', soloAdmin, (req, res)=>{
+    const query = 'INSERT INTO productos VALUES (NULL,?, ?, ?)';
+    const {nombre, imagen, precio} = req.body;
+    sequelize.query(query, {replacements: [nombre, imagen, precio]})
+        .then ((response) => {
+            res.status(200).send('Producto agregado a la base de datos');
+        }).catch(e => console.error('No se pudo agregar producto', e), res.status(400));
+});
+
+//Actualizar producto
+
+app.put('/api/productos/:id', soloAdmin, (req, res) => {
+    const query = 'UPDATE productos SET nombre = ?, imagen = ?, precio =? WHERE id_producto = ?';
+    const id = req.params.id;
+    const nombre = req.body.nombre;
+    const imagen = req.body.imagen;
+    const precio = req.body.precio;    
+    sequelize.query(query, {replacements: [nombre, imagen, precio,id] })
+        .then((response) => {
+            res.status(200).send('Producto actualizado');
+        }).catch((e) => res.status(400).json({error: 'No se pudo actualizar Producto'}));
+});
+
+//Eliminar producto
+
+app.delete('/api/productos/:id', soloAdmin,(req, res) => {
+    const id = req.params.id;
+    const myQuery = 'DELETE FROM productos WHERE id_producto = ?';
+    const query = 'SELECT * FROM productos WHERE id_producto = ?';
+    sequelize.query(myQuery, { replacements: [id] })
+                        .then((data) => {
+                             res.status(200).send('Producto eliminado correctamente');
+                        }).catch((e) => res.status(400).json({error: 'No se pudo eliminar producto'}));
+/*
+    sequelize.query(query, {replacements:[id], type: sequelize.QueryTypes.SELECT})
+        .then((response)=>{ 
+            if(response.length){
+                sequelize.query(myQuery, { replacements: [id] })
+                        .then((data) => {
+                             res.status(200).send('Producto eliminado correctamente');
+                        }).catch((e) => res.status(400).json({error: 'No se pudo eliminar producto'}));         
+            } else {
+                res.status(404).send('Producto inexistente'); 
+            }
+        }).catch((e) => res.status(400).json({error: 'No se pudo eliminar producto'}));
+        */
+});
+
+//USUARIOS
+
+//Mostrar Usuarios
+
+app.get('/api/usuarios', soloAdmin, (req, res)=> {
+    const query = 'SELECT * FROM usuarios';
+    sequelize.query(query, {type: sequelize.QueryTypes.SELECT})
+        .then((response)=>{
+            res.status(200).json(response);
+        }).catch((e) => res.status(400).json({error: 'No se pudo mostrar usuarios'}));
+});
+
+//Mostrar un usuario (Para ver su propio usuario) En el front se muestra su id
+
+app.get('/api/usuarios/:id', ensureToken,(req, res)=> {
+    const id = req.params.id;
+    const query = 'SELECT * FROM usuarios WHERE id_usuario = ?';
+    sequelize.query(query, {replacements: [id], type: sequelize.QueryTypes.SELECT})
+        .then((response)=>{
+            res.json(response);
+            console.log(response);
+        }).catch((e) => res.status(400).json({error: 'No se puede mostrar el usuario'}));
+});
+
+//Agregar usuario (El campo Administrador es por defecto 0)      
+
+app.post('/api/usuarios', existeUsuario,(req, res)=>{
+    const query = 'INSERT INTO usuarios VALUES (NULL, ?, ?, ?, ?, ?, ?, 0)';
+    const {nombre_usuario, nombre_apellido, email, telefono, direccion, contrasena} = req.body;
+    console.log(nombre_usuario, nombre_apellido, email, telefono, direccion, contrasena);
+            if (!nombre_usuario || !nombre_apellido|| !email || !telefono || !direccion || !contrasena){ 
+                res.status(401).json({error: 'Faltan campos obligatorios'});
+            } else {    
+                sequelize.query(query, {replacements: [nombre_usuario, nombre_apellido, email, telefono, direccion, contrasena]})
+                    .then ((response) => {   
+                        res.status(200).send('Usuario agregado correctamente');
+                    }).catch(e => console.error('No se pudo agregar usuario', e), res.status(400));
+             }
+});
+
+//Eliminar usuario
+
+app.delete('/api/usuarios/:id', soloAdmin, (req, res) => {
+    const id = req.params.id;
+    const myQuery = 'DELETE FROM usuarios WHERE id_usuario = ?';
+    const query = 'SELECT * FROM usuarios WHERE id_usuario = ?';
+    sequelize.query(query, {replacements:[id], type: sequelize.QueryTypes.SELECT})
+        .then((response)=>{ 
+            if(response.length){
+                sequelize.query(myQuery, { replacements: [id] })
+                        .then((data) => {
+                            console.log(data);
+                             res.status(200).send('Usuario eliminado correctamente');
+                        }).catch((e) => res.status(400).json({error: 'No se pudo eliminar usuario'}));         
+            } else {
+                res.status(404).send('Usuario inexistente'); 
+            }
+        }).catch((e) => res.status(400).json({error: 'Algo salió mal'}));
+});
+
+//Actualizar Usuario (para cambiar usuario administrador u otro campo)
+
+app.put('/api/usuarios/:id', soloAdmin, (req, res) => {
+    const query = 'UPDATE usuarios SET nombre_usuario =?, nombre_apellido =?, email = ?, telefono =?, direccion = ?, contrasena =?, es_admin = ? WHERE id_usuario=?';
+    const id = req.params.id;
+    const nombre = req.body.nombre_usuario; 
+    const apellido = req.body.nombre_apellido; 
+    const email = req.body.email; 
+    const telefono = req.body.telefono; 
+    const direccion =req.body.direccion;
+    const contrasena = req.body.contrasena; 
+    const es_admin = req.body.es_admin;
+    sequelize.query(query, {replacements: [nombre, apellido, email, telefono, direccion, contrasena, es_admin, id]})
+        .then((response) => {(response)
+            res.status(200).send('Usuario actualizado correctamente');
+        }).catch(e => console.error('No se modificó usuario', e), res.status(400));
+});
+
+//PEDIDOS
+
+//Mostrar todos los datos de todos los pedidos
+
+app.get('/api/pedidos', soloAdmin, (req, res) => {
+    const query = 'SELECT estado, hora, pedidos.id_pedido As Número, nombre, cantidad, metodo_pago, total, nombre_usuario, direccion FROM usuarios JOIN pedidos join productos_x_pedidos JOIN productos ON pedidos.id_usuario = usuarios.id_usuario AND pedidos.id_pedido = productos_x_pedidos.id_pedido AND productos_x_pedidos.id_producto = productos.id_producto';
+    sequelize.query(query, { type: sequelize.QueryTypes.SELECT })
+        .then((response)=>{
+        res.status(200).json(response);
+        }).catch(e => console.error('No se pudo mostrar pedido', e), res.status(400));
+});
+
+//Agregar pedidos (Por defecto el estado siempre es "Nuevo")
+
+app.post('/api/pedidos', ensureToken, (req, res) => {
+    const query = 'INSERT INTO pedidos VALUES (NULL, ?, ?, "Nuevo", ?, ?, ?)';
+    const {hora, estado, total, metodo_pago, id_usuario } = req.body;
+    jwt.verify(req.token, passwordJwt, (error, data) => {
+        if (error) {
+            res.sendStatus(403).json('Debes iniciar sesión');
+        } else {
+            if(!hora || !estado || !total || !metodo_pago || !id_usuario){
+                res.status(400).send('Faltan campos obligatorios');
+            } else{
+            sequelize.query(query, { replacements: [hora, total, metodo_pago, id_usuario] })
+                .then((response) => {
+                    res.status(200).send('Pedido agregado correctamente');
+            }).catch(e => console.error('No se pudo agregar pedido a la base de datos', e), res.status(400));
+         }}
+    }) 
+});
+
+//Mostrar pedido por id
+
+app.get('/api/pedidos/:id', ensureToken, (req, res) => {
+    const id = req.params.id;
+    const query = 'SELECT estado, hora, pedidos.id_pedido As Número, nombre, cantidad, metodo_pago, total, nombre_usuario, direccion FROM usuarios JOIN pedidos join productos_x_pedidos JOIN productos ON pedidos.id_usuario = usuarios.id_usuario AND pedidos.id_pedido = productos_x_pedidos.id_pedido AND productos_x_pedidos.id_producto = productos.id_producto WHERE pedidos.id_pedido = ?';
+    jwt.verify(req.token, passwordJwt, (error, data) => {
+        if (error) {
+            res.sendStatus(403).json('Debes iniciar sesión');
+        } else {
+        sequelize.query(query, { replacements: [id], type: sequelize.QueryTypes.SELECT })
+            .then((data) => {
+                res.status(200).json(data)
+            }).catch(e => console.error('No se pudo mostrar pedidos', e), res.status(400));        
+        }
+    })
+});   
+    
+//Eliminar pedido
+
+app.delete('/api/pedidos/:id', soloAdmin, (req, res) => {
+    const id = req.params.id;
+    const myQuery = 'DELETE FROM pedidos WHERE id_pedido =?';
+    sequelize.query(myQuery, {replacements: [id] })
+        .then((data) => {
+            res.status(200).send('Pedido eliminado correctamente');
+        }).catch(e => console.error('No se pudo eliminar pedido', e), res.status(400));  
+});
+
+//Actualizar pedidos (para actualizar el estado del pedido u otro campo que desee el Administrador)
+
+app.put('/api/pedidos/:id', soloAdmin,(req, res) => {
+    const id = req.params.id;
+    //const descripcion = req.body.descripcion;
+    const hora= req.body.hora;
+    const estado = req.body.estado;
+    const total = req.body.total;
+    const metodo_pago = req.body.metodo_pago;
+    const id_usuario = req.body.id_usuario;
+    const myQuery = `UPDATE pedidos SET hora = ?, estado = ?, total = ?, metodo_pago = ?, id_usuario = ?  WHERE id_pedido = ?`;
+    sequelize.query(myQuery, { replacements: [hora, estado, total, metodo_pago, id_usuario, id] })
+        .then((data) => {
+            res.status(200).send('Pedido actualizado correctamente');
+        }).catch(e => console.error('No se pudo actualizar producto', e), res.status(400));
+});
+
+//Agregar productos (para el ingreso de la cantidad de un producto)
+
+app.post('/api/productos_x_pedidos', ensureToken, (req, res) => {
+    const query = 'INSERT INTO productos_x_pedidos VALUES (NULL, ?, ?, ?)';
+    const {id_producto, id_pedido, cantidad} = req.body;
+    jwt.verify(req.token, passwordJwt, (error, data) => {   
+        if (error) {
+            res.sendStatus(403).json('Debe iniciar sesión');
+        } else {
+            sequelize.query(query, { replacements: [id_producto, id_pedido, cantidad] })
+            .then((response) => {
+                res.status(200).send('Producto agregado correctamente');
+            }).catch(e => console.error('No se pudo agregar producto', e), res.status(400));
+         }
+    })
+});
+
+//Login
+
+app.post('/login', async (req, res) => {
+    const {email, contrasena} = req.body;
+    await sequelize .query('SELECT * FROM usuarios WHERE email = ? AND contrasena = ?', {
+        type: sequelize.QueryTypes.SELECT, replacements: [email, contrasena],
+    })
+    .then(async (user) => {
+        let usuario = user[0]
+        if (usuario === undefined) {
+            res.status(403).send('Email o contraseña incorrectos');
+        } else {
+            const token = jwt.sign(usuario, passwordJwt)
+            res.status(200).json({token : token})
+        }
+    })
+})
+
+function ensureToken(req, res, next){
+    const bearerHeader = req.headers['authorization'];
+    console.log(bearerHeader);
+    if(typeof bearerHeader !== 'undefined') {
+        const bearer = bearerHeader.split(" ");    
+        const bearerToken = bearer[1];              
+        req.token = bearerToken;
+        next();
+    } else {
+        res.sendStatus(403).json('Token inválido');
+    }
+}
+
+//Con esta ruta se accede seguro (Queda a criterio del desarrollo de front usarla)
+
+app.get('/api/seguro', ensureToken, (req, res) => {
+    jwt.verify(req.token, passwordJwt, (error, data) => {
+        if (error) {
+            res.sendStatus(403).json('Debes iniciar sesión');
+        } else {
+            res.json({
+                data, text: 'Usuario protegido'
+            })
+        }
+    });
+});
+
+function soloAdmin(req, res, next) {
+    const autorizacion = req.headers['authorization'];
+    const token = autorizacion && autorizacion.split(' ')[1];
+    if (token == null) return res.sendStatus(401).send('Token inexistente');
+    jwt.verify(token, passwordJwt, (err, usuario) => {
+        if (err) {
+            return res.sendStatus(403).send('Error de autenticación');
+        }
+        const user = usuario;
+        console.log(usuario);
+        sequelize.query('SELECT * FROM usuarios WHERE id_usuario = ? ', {
+                replacements: [user.id_usuario], type: sequelize.QueryTypes.SELECT
+            }).then((usuarios) => {
+                 if (usuarios[0].es_admin === 1) {
+                    next();
+                } else {
+                    res.status(403).send('No tienes permisos de Administrador');
+                 }
+            })
+    })
+}
+
+function existeUsuario(req, res, next){
+    const usuario = req.body.nombre_usuario;
+    sequelize.query('SELECT * FROM usuarios WHERE nombre_usuario = ?',{
+        type: sequelize.QueryTypes.SELECT, replacements: [usuario]
+    })
+    .then ((user) => {
+        if(user.length) {
+           res.status(403).send('Usuario no disponible. Elija otro por favor.');               
+        }
+        else {
+           next();
+         }
+        });
+}
+
+app.listen(4000, function(request, response){console.log('La app está corriendo...')});
